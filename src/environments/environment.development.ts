8ed9fce665ffdc4e8c313afb556f8b39deb1f63c
YoesDevelop <ceticay7@gmail.com>
2024-06-14 10:46:43 -0500

    update links

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: false,
-  serverBasePath: 'https://minimarket-yoes.zeabur.app/api/v1/product',
-  serverRegister: 'https://minimarket-yoes.zeabur.app/api/minimarket/usuarios',
-  serverRegister2: 'https://minimarket-yoes.zeabur.app/api/minimarket/usuarios',
-
-  /*
-  *  serverBasePath: 'https://backend-yoes-final.onrender.com/api/v1/product',
-  serverRegister: 'https://backend-yoes-final.onrender.com/api/minimarket/usuarios',
-  serverRegister2: 'https://backend-yoes-final.onrender.com/api/minimarket/usuarios',*/
-
+  serverBasePath: 'https://backend-minimarket-yoes-tabs.onrender.com/api/v1/product',
+  serverRegister: 'https://backend-minimarket-yoes-tabs.onrender.com/api/minimarket/usuarios',
+  serverRegister2: 'https://backend-minimarket-yoes-tabs.onrender.com/api/minimarket/usuarios',
 
 }
